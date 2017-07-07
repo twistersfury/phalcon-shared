758f3fa83bddeb84917c292efa287c05dcdd73b8
Phoenix Osiris <blogger@twistersfury.com>
2017-07-06 22:49:56 -0400

    Removing Previously 'Smart' Logic To Ensure Proper Initial Setup

--- src/phalcon_bootstrap.php
+++ src/phalcon_bootstrap.php
@@ -10,6 +10,7 @@
     namespace TwistersFury\Phalcon\Shared;
 
     use Phalcon\Di;
+    use Phalcon\Di\FactoryDefault;
     use Phalcon\Events\Event;
     use Phalcon\Loader;
     use TwistersFury\Phalcon\Shared\Helpers\Defines;
@@ -21,51 +22,37 @@
         ]
     )->register(true);
 
-    //If We are In CLI Mode, Chances Are We Are In Testing Mode
-    if (PHP_SAPI !== 'cli') {
-        if (!Di::getDefault()) {
-            throw new \LogicException("Phalcon Di Not Yet Configured");
-        } else if (!Di::getDefault()->has('eventsManager')) {
-            throw new \LogicException("Phalcon Di Events Manager Not Yet Configured");
-        }
-    } else {
-        $factoryDefault = new \Phalcon\Di\FactoryDefault();
-        $factoryDefault->setShared('eventsManager', \Phalcon\Events\Manager::class);
-        $factoryDefault->get('eventsManager')->attach('twistersfury::dynamic-defines', function(\TwistersFury\Phalcon\Shared\Helpers\Defines $defines) {
-            $defines->define('TF_DEBUG_MODE', TF_DEBUG_MODE_TESTING);
-        });
 
-        \Phalcon\Di::setDefault($factoryDefault);
-
-        unset($factoryDefault);
+    if (!Di::getDefault()) {
+        Di::setDefault((new FactoryDefault()));
     }
 
-    /** @var \Phalcon\Events\Manager $eventsManager */
-    $eventsManager = Di::getDefault()->get('eventsManager');
+    (function() {
+        /** @var \Phalcon\Events\Manager $eventsManager */
+        $eventsManager = Di::getDefault()->get('eventsManager');
 
-    $eventsManager->attach('twistersfury:static-defines', function(Event $event, Defines $definesHelper) {
-        $definesHelper->define('TF_DEBUG_MODE_DISABLED', 0)
-            ->define('TF_DEBUG_MODE_TESTING' , 1);
-    });
+        $eventsManager->attach('twistersfury:static-defines', function(Event $event, Defines $definesHelper) {
+            $definesHelper->define('TF_DEBUG_MODE_DISABLED', 0)
+                ->define('TF_DEBUG_MODE_TESTING' , 1);
+        });
 
-    $eventsManager->attach('twistersfury:dynamic-defines', function(Event $event, Defines $definesHelper) {
-        $definesHelper->define(
-            'TF_SHARED_SOURCE',
-            __DIR__
-        )->define(
-            'TF_SHARED_PROJECT',
-            realpath(__DIR__ . '/..')
-        )->define(
-            'TF_SHARED_TESTS',
-            realpath(__DIR__ . '/../tests/unit')
-        )->define('TF_APP_ROOT', function() {
-            $isComposer = strstr(__DIR__, 'vendor') !== false;
+        $eventsManager->attach('twistersfury:dynamic-defines', function(Event $event, Defines $definesHelper) {
+            $definesHelper->define(
+                'TF_SHARED_SOURCE',
+                __DIR__
+            )->define(
+                'TF_SHARED_PROJECT',
+                realpath(__DIR__ . '/..')
+            )->define(
+                'TF_SHARED_TESTS',
+                realpath(__DIR__ . '/../tests/unit')
+            )->define('TF_APP_ROOT', function() {
+                $isComposer = strstr(__DIR__, 'vendor') !== false;
 
-            return $isComposer ? __DIR__ . '/../../../app' : __DIR__ . '/..';
-        })->define('TF_DEBUG_MODE', getenv('TF_DEBUG_MODE') ?: TF_DEBUG_MODE_DISABLED);
-    }, -1000);
+                return $isComposer ? __DIR__ . '/../../../app' : __DIR__ . '/..';
+            })->define('TF_DEBUG_MODE', getenv('TF_DEBUG_MODE') ?: TF_DEBUG_MODE_DISABLED);
+        }, -1000);
 
-    $eventsManager->fire('twistersfury:static-defines', Di::getDefault()->get(Defines::class));
-    $eventsManager->fire('twistersfury:dynamic-defines', Di::getDefault()->get(Defines::class));
-
-    unset($eventsManager);
+        $eventsManager->fire('twistersfury:static-defines', Di::getDefault()->get(Defines::class));
+        $eventsManager->fire('twistersfury:dynamic-defines', Di::getDefault()->get(Defines::class));
+    })();
