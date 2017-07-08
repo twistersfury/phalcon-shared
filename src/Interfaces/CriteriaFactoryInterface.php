81246cc452ccf37645fc4f98127678c1cdacfaf3
Phoenix Osiris <blogger@twistersfury.com>
2017-07-08 15:18:06 -0400

    Addition of CriteriaFactoryInterface

---
+++ src/Interfaces/CriteriaFactoryInterface.php
@@ -0,0 +1,13 @@
+<?php
+    /**
+     * Created by PhpStorm.
+     * User: fenikkusu
+     * Date: 7/8/17
+     * Time: 12:44 AM
+     */
+
+    namespace TwistersFury\Phalcon\Authentication\Interfaces;
+
+    interface CriteriaFactoryInterface {
+
+    }
