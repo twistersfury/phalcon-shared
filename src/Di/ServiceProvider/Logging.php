d8d75ac0465531ee2e7eba289cffc0b8c47675c5
Fenikkusu <phoenix@twistersfury.com>
2018-08-19 23:44:14 -0400

    Adding Logging Service Provider

---
+++ src/Di/ServiceProvider/Logging.php
@@ -0,0 +1,113 @@
+<?php
+/**
+ * Copyright (C) 2018 Twister's Fury.
+ * Distributed under the MIT License (license terms are at http://opensource.org/licenses/MIT).
+ */
+
+namespace TwistersFury\Phalcon\Shared\Di\ServiceProvider;
+
+use Monolog\Formatter\HtmlFormatter;
+use Monolog\Handler\BufferHandler;
+use Monolog\Handler\FingersCrossedHandler;
+use Monolog\Handler\HandlerInterface;
+use Monolog\Handler\RotatingFileHandler;
+use Monolog\Handler\SwiftMailerHandler;
+use Monolog\Logger;
+
+class Logging extends AbstractServiceProvider
+{
+    protected function registerLogger() : self
+    {
+        $this->setShared('logger', function () {
+            /** @var \Phalcon\Config $servicesConfig */
+            $servicesConfig = $this->get('config')->services;
+
+            /** @var \Swift_Message $exampleMessage */
+            $exampleMessage = $this->get(
+                \Swift_Message::class,
+                [
+                    'Critical Server Errors',
+                    '',
+                    'text/html'
+                ]
+            );
+
+            $exampleMessage->addTo(
+                $servicesConfig->logging->to
+            )->addFrom($servicesConfig->mail->from);
+
+            $emailHandler = $this->get(
+                SwiftMailerHandler::class,
+                [
+                    \Swift_Mailer::newInstance(
+                        \Swift_SmtpTransport::newInstance(
+                            $servicesConfig->mail->host
+                        )
+                    ),
+                    $exampleMessage,
+                    Logger::DEBUG
+                ]
+            );
+
+            $debugLevel = $servicesConfig->logging->env_levels->get(
+                $this->get('config')->environment
+            ) ?: $servicesConfig->logging->env_levels->get('default');
+
+            $emailHandler->setFormatter(
+                $this->get(HtmlFormatter::class)
+            );
+
+            /** @var Logger $logger */
+            $logger = $this->get(
+                Logger::class,
+                [
+                    'app-logging'
+                ]
+            )->pushHandler(
+                $this->get(
+                    RotatingFileHandler::class,
+                    [
+                        $this->get('config')->application->get(
+                            'logFile',
+                            $this->get('config')->application->logDir
+                        ) . '/logging-app.log',
+                        10,
+                        $debugLevel
+                    ]
+                )
+            )->pushHandler(
+                new FingersCrossedHandler(
+                    $this->get(
+                        'bufferHandler',
+                        [
+                            $emailHandler
+                        ]
+                    ),
+                    $debugLevel
+                )
+            );
+
+            return $logger;
+        });
+
+        return $this;
+    }
+
+    protected function registerBufferHandler()
+    {
+        $this->setShared(
+            'bufferHandler',
+            function (HandlerInterface $handler) {
+                /** @var BufferHandler $bufferHandler */
+                $bufferHandler = $this->get(BufferHandler::class, [$handler]);
+                register_shutdown_function(
+                    function () use ($bufferHandler) {
+                        $bufferHandler->close();
+                    }
+                );
+
+                return $bufferHandler;
+            }
+        );
+    }
+}
